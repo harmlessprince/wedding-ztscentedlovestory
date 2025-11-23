da3f9747d2b50fac934d47c4892f9765a3978ee2
harmlessprince <realolamilekan@gmail.com>
2025-11-23 17:30:31 +0100

    updated mail

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -186,18 +186,6 @@
 
 
                 <div id="groomContent" class="p-6 space-y-4">
-                    <div>
-                        <p class="text-maroon text-sm mb-1">Account name</p>
-                        <div class="flex items-center justify-between bg-white p-3 rounded-lg">
-                            <p class="text-maroon font-semibold">Taofeeq Olamilekan Adewuyi</p>
-                            <button class="copy-btn text-maroon" data-copy="Taofeeq Olamilekan Adewuyi">
-                                <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                          d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z"></path>
-                                </svg>
-                            </button>
-                        </div>
-                    </div>
 
                     <div>
                         <p class="text-maroon text-sm mb-1">Account number</p>
@@ -212,35 +200,36 @@
                         </div>
                     </div>
 
+
+                    <div>
+                        <p class="text-maroon text-sm mb-1">Account name</p>
+                        <div class="flex items-center justify-between bg-white p-3 rounded-lg">
+                            <p class="text-maroon font-semibold">Taofeeq Olamilekan Adewuyi</p>
+{{--                            <button class="copy-btn text-maroon" data-copy="Taofeeq Olamilekan Adewuyi">--}}
+{{--                                <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">--}}
+{{--                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"--}}
+{{--                                          d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z"></path>--}}
+{{--                                </svg>--}}
+{{--                            </button>--}}
+                        </div>
+                    </div>
+
                     <div>
                         <p class="text-maroon text-sm mb-1">Bank name</p>
                         <div class="flex items-center justify-between bg-white p-3 rounded-lg">
                             <p class="text-maroon font-semibold">OPay Digital Services Limited(OPay)</p>
-                            <button class="copy-btn text-maroon" data-copy="OPay Digital Services Limited(OPay)">
-                                <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                          d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z"></path>
-                                </svg>
-                            </button>
+{{--                            <button class="copy-btn text-maroon" data-copy="OPay Digital Services Limited(OPay)">--}}
+{{--                                <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">--}}
+{{--                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"--}}
+{{--                                          d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z"></path>--}}
+{{--                                </svg>--}}
+{{--                            </button>--}}
                         </div>
                     </div>
                 </div>
 
 
                 <div id="brideContent" class="p-6 space-y-4 hidden">
-                    <div>
-                        <p class="text-maroon text-sm mb-1">Account name</p>
-                        <div class="flex items-center justify-between bg-white p-3 rounded-lg">
-                            <p class="text-maroon font-semibold">Zuliat Ismail-Bello</p>
-                            <button class="copy-btn text-maroon" data-copy="Zuliat Ismail-Bello">
-                                <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                          d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z"></path>
-                                </svg>
-                            </button>
-                        </div>
-                    </div>
-
                     <div>
                         <p class="text-maroon text-sm mb-1">Account number</p>
                         <div class="flex items-center justify-between bg-white p-3 rounded-lg">
@@ -253,17 +242,30 @@
                             </button>
                         </div>
                     </div>
+                    <div>
+                        <p class="text-maroon text-sm mb-1">Account name</p>
+                        <div class="flex items-center justify-between bg-white p-3 rounded-lg">
+                            <p class="text-maroon font-semibold">Zuliat Ismail-Bello</p>
+{{--                            <button class="copy-btn text-maroon" data-copy="Zuliat Ismail-Bello">--}}
+{{--                                <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">--}}
+{{--                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"--}}
+{{--                                          d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z"></path>--}}
+{{--                                </svg>--}}
+{{--                            </button>--}}
+                        </div>
+                    </div>
+
 
                     <div>
                         <p class="text-maroon text-sm mb-1">Bank name</p>
                         <div class="flex items-center justify-between bg-white p-3 rounded-lg">
                             <p class="text-maroon font-semibold">PocketApp</p>
-                            <button class="copy-btn text-maroon" data-copy="PocketApp">
-                                <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                          d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z"></path>
-                                </svg>
-                            </button>
+{{--                            <button class="copy-btn text-maroon" data-copy="PocketApp">--}}
+{{--                                <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">--}}
+{{--                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"--}}
+{{--                                          d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z"></path>--}}
+{{--                                </svg>--}}
+{{--                            </button>--}}
                         </div>
                     </div>
                 </div>
