208e5c23af842efab2644d51751a3edae92c085a
harmlessprince <realolamilekan@gmail.com>
2025-11-23 17:37:43 +0100

    updated mail

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -135,11 +135,11 @@
                             d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z"></path>
                     </svg>
                     <div>
-                        <p class="text-maroon text-xs">R.S.V.P (Quadri)</p>
-                        <p class="text-maroon font-bold">09058695294</p>
+                        <p class="text-maroon text-xs">Mariam (bride)</p>
+                        <p class="text-maroon font-bold">07062823227</p>
                     </div>
                 </div>
-                <button class="copy-btn text-maroon" data-copy="09058695294">
+                <button class="copy-btn text-maroon" data-copy="07062823227">
                     <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                               d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z"></path>
@@ -154,7 +154,7 @@
                             d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z"></path>
                     </svg>
                     <div>
-                        <p class="text-maroon text-xs">R.S.V.P (Quadri)</p>
+                        <p class="text-maroon text-xs">Quadri (groom)</p>
                         <p class="text-maroon font-bold">09058695294</p>
                     </div>
                 </div>
