c3eedd346d996ebb9989fb42dfae76565700887a
harmlessprince <realolamilekan@gmail.com>
2025-12-07 20:05:02 +0100

    added field to accept number of children

--- app/Models/Rsvp.php
+++ app/Models/Rsvp.php
@@ -6,16 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Rsvp extends Model
 {
-    protected $fillable = [
-        'surname',
-        'first_name',
-        'email',
-        'phone',
-        'side',
-        'type',
-        'message',
-        'hash',
-        'invite_card_url',
-        'invite_code',
-    ];
+    protected $guarded = [];
 }
