b1045112c735e4cf5b0afaec142cf6026739084b
harmlessprince <realolamilekan@gmail.com>
2025-12-10 18:08:47 +0100

    relaxed bot

--- routes/api.php
+++ routes/api.php
@@ -28,12 +28,12 @@ Route::post('/get-ticket', function (Request $request) {
 
     $captcha = $validated['captcha'];
     $score = RecaptchaV3::verify($captcha, 'register');
-//    if ($score < 0.5) {
-//        return response()->json([
-//            'success' => false,
-//            'message' => 'You are most likely a bot, kindly refresh the page and try again.',
-//        ]);
-//    }
+    if ($score < 0.5) {
+        return response()->json([
+            'success' => false,
+            'message' => 'You are most likely a bot, kindly refresh the page and try again.',
+        ]);
+    }
     unset($validated['captcha']);
     $rsvp = Rsvp::query()->where('phone', $validated['phone'])->first();
     if ($rsvp) {
